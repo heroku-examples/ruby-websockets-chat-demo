3cd853e0dd14cd2092594aaaa14eb8db7dfb08e7
Terence Lee <hone02@gmail.com>
2013-09-28 03:22:37 -0400

    add keepalive ping

--- app/middleware/chat_backend.rb
+++ app/middleware/chat_backend.rb
@@ -1,9 +1,11 @@
 require 'faye/websocket'
 
 class ChatBackend
+  KEEPALIVE_TIME = 30 # in seconds
+
   def initialize(app)
     @app     = app
-    @clients = []
+    @clients = {}
   end
 
   def call(env)
@@ -12,16 +14,20 @@ class ChatBackend
 
       ws.on :open do |event|
         p [:open, ws.object_id]
-        @clients << ws
+        @clients[ws] = EM::PeriodicTimer.new(KEEPALIVE_TIME) do
+          p [:ping, ws.object_id]
+          ws.ping
+        end
       end
 
       ws.on :message do |event|
         p [:message, event.data]
-        @clients.each {|client| client.send(event.data) }
+        @clients.keys.each {|client| client.send(event.data) }
       end
 
       ws.on :close do |event|
         p [:close, ws.object_id, event.code, event.reason]
+        @clients[ws].cancel
         @clients.delete(ws)
         ws = nil
       end
