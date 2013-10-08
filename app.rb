9b4d0b6866c86e88dd5edc9daa9cbee389a995e8
Terence Lee <hone02@gmail.com>
2013-10-08 01:57:53 -0400

    use wss:// in production

--- app.rb
+++ app.rb
@@ -5,5 +5,11 @@ module ChatDemo
     get "/" do
       erb :"index.html"
     end
+
+    get "/assets/js/application.js" do
+      content_type :js
+      @scheme = ENV['RACK_ENV'] == "production" ? "wss://" : "ws://"
+      erb :"application.js"
+    end
   end
 end
