9a644db07a6f2c92a9481c6f59c356c23f1c1430
kojii <saito@grepfind.co>
2014-12-17 21:09:44 +0900

    力まないシステムプロトタイプ

--- app.rb
+++ app.rb
@@ -3,13 +3,23 @@ require 'sinatra/base'
 module ChatDemo
   class App < Sinatra::Base
     get "/" do
-      erb :"index.html"
+      slim :"index.html"
+    end
+
+    get "/main" do
+      slim :"main.html"
     end
 
     get "/assets/js/application.js" do
       content_type :js
       @scheme = ENV['RACK_ENV'] == "production" ? "wss://" : "ws://"
       erb :"application.js"
+    end
+
+    get "/assets/js/button.js" do
+      content_type :js
+      @scheme = ENV['RACK_ENV'] == "production" ? "wss://" : "ws://"
+      erb :"button.js"
     end
   end
 end
--- middlewares/chat_backend.rb
+++ middlewares/chat_backend.rb
@@ -1,13 +1,20 @@
+# -*- coding: utf-8 -*-
 require 'faye/websocket'
 require 'thread'
 require 'redis'
 require 'json'
-require 'erb'
+require 'slim'
 
 module ChatDemo
   class ChatBackend
     KEEPALIVE_TIME = 15 # in seconds
-    CHANNEL        = "chat-demo"
+    CHANNEL = "rikimanai-demo"
+    NAME = {
+      "shuhei" => "しゅうへい",
+      "kakizaki" => "柿崎",
+      "abe" => "あべ",
+      "koji" => "こうじ"
+    }
 
     def initialize(app)
       @app     = app
@@ -18,7 +25,11 @@ module ChatDemo
         redis_sub = Redis.new(host: uri.host, port: uri.port, password: uri.password)
         redis_sub.subscribe(CHANNEL) do |on|
           on.message do |channel, msg|
-            @clients.each {|ws| ws.send(msg) }
+            p "subscribe: count=#{@clients.count} : #{msg}"
+            @clients.each {|ws|
+              p "WebSocket:#{ws}"
+              ws.send(msg)
+            }
           end
         end
       end
@@ -26,15 +37,50 @@ module ChatDemo
 
     def call(env)
       if Faye::WebSocket.websocket?(env)
-        ws = Faye::WebSocket.new(env, nil, {ping: KEEPALIVE_TIME })
+        ws = Faye::WebSocket.new(env, ping: 15, retry: 10)
         ws.on :open do |event|
           p [:open, ws.object_id]
           @clients << ws
+          @threshold = @redis.get "threshold"
+          json = JSON.generate({
+                                 command: "init",
+                                 opt: {
+                                   threshold: @threshold,
+                                   counts: {
+                                     shuhei: @redis.get("vote-shuhei"),
+                                     kakizaki: @redis.get("vote-kakizaki"),
+                                     abe: @redis.get("vote-abe"),
+                                     koji: @redis.get("vote-koji")
+                                   }
+                                 }
+                               })
+          puts json
+          @redis.publish CHANNEL, json
         end
 
         ws.on :message do |event|
           p [:message, event.data]
-          @redis.publish(CHANNEL, sanitize(event.data))
+          data  = JSON.parse(event.data)
+          case data["command"]
+          when "rikinderu"
+            count = @redis.incr("vote-#{data["target"]}")
+            data["count"] = count
+            @redis.publish(CHANNEL, sanitize(data))
+            p "count=#{count}, threshold=#{@threshold}"
+            if count >= @threshold
+              target = data["target"]
+              data["command"] = "out"
+              data["name"] = NAME[target]
+              @redis.publish(CHANNEL, sanitize(data))
+              @redis.set data["vote-#{target}"], 0
+            end
+          when "threshold"
+            @threshold = data["val"].to_i
+            @redis.set "threshold", @threshold
+          when "reset"
+            reset
+            @redis.publish(CHANNEL, sanitize(data))
+          end
         end
 
         ws.on :close do |event|
@@ -52,10 +98,15 @@ module ChatDemo
     end
 
     private
-    def sanitize(message)
-      json = JSON.parse(message)
-      json.each {|key, value| json[key] = ERB::Util.html_escape(value) }
+    def sanitize(json)
+      #json.each {|key, value| json[key] = ERB::Util.html_escape(value) }
       JSON.generate(json)
+    end
+
+    def reset
+      @redis.keys("vote-*").each do |key|
+        @redis.set key, 0
+      end
     end
   end
 end
